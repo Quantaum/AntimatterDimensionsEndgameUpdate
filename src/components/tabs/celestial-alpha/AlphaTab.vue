c287c22bea7cc3c9fc6134045a07f920fe26583d
Quantaum <crescentiumlmao@gmail.com>
2026-01-19 02:17:00 -0800

    Update AlphaTab.vue

---
+++ src/components/tabs/celestial-alpha/AlphaTab.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="l-alpha-tab">
+    <div class="l-alpha-content">
+      <h2>Alpha</h2>
+      <p>Alpha tab - Work in progress</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlphaTab",
+  data() {
+    return {
+    };
+  },
+  computed: {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped>
+.l-alpha-tab {
+  width: 100%;
+}
+
+.l-alpha-content {
+  padding: 1rem;
+}
+</style>
