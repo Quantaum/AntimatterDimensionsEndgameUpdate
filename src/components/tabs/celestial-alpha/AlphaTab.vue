b5bd710e6f2740d04ad7fd3fc89416e261a395af
Quantaum <crescentiumlmao@gmail.com>
2026-01-19 14:41:13 -0800

    Update AlphaTab.vue

--- src/components/tabs/celestial-alpha/AlphaTab.vue
+++ src/components/tabs/celestial-alpha/AlphaTab.vue
@@ -1,34 +1,45 @@
 <script>
-import { DC } from "@/core/constants";
-
 import CelestialQuoteHistory from "@/components/CelestialQuoteHistory";
-import CustomizeableTooltip from "@/components/CustomizeableTooltip";
-import GlyphSetPreview from "@/components/GlyphSetPreview";
-import PrimaryButton from "@/components/PrimaryButton";
 
 export default {
   name: "AlphaTab",
   components: {
-    PrimaryButton,
-    GlyphSetPreview,
     CelestialQuoteHistory,
-    CustomizeableTooltip
   },
   data() {
     return {
       isRunning: false,
-      isDoomed: false,
     };
   },
   computed: {
-    runDescription() {
-      return GameDatabase.celestials.descriptions[7].effects();
+    runButtonOuterClass() {
+      return {
+        "l-alpha-run-button": true,
+        "c-alpha-run-button": true,
+        "c-alpha-run-button--running": this.isRunning,
+        "c-alpha-run-button--not-running": !this.isRunning,
+        "c-celestial-run-button--clickable": !this.isDoomed,
+        "o-pelle-disabled-pointer": this.isDoomed
+      };
     },
+    runButtonInnerClass() {
+      return this.isRunning ? "c-alpha-run-button__inner--running" : "c-alpha-run-button__inner--not-running";
+    },
+    runDescription() {
+      return `${GameDatabase.celestials.descriptions[7].effects()}\n
+      ${GameDatabase.celestials.descriptions[7].description()}`;
+    },
+    symbol: () => "Ω",
+    isDoomed: () => Pelle.isDoomed,
+  },
+  watch: {
+    isRunning() {
+      this.$recompute("runDescription");
+    }
   },
   methods: {
     update() {
       this.isRunning = Alpha.isRunning;
-      this.isDoomed = Pelle.isDoomed;
     },
     startRun() {
       if (this.isDoomed) return;
@@ -39,72 +50,34 @@ export default {
 </script>
 
 <style scoped>
-.l-alpha-tab {
-  width: 100%;
-}
-
-.l-alpha-content {
-  padding: 1rem;
-}
-
-.c-alpha-run-button {
-  width: 100%;
-  padding: 1.5rem;
-  margin: 1rem 0;
-  background: linear-gradient(135deg, #1a1a1a, #2d2d2d);
-  border: 2px solid #444;
-  border-radius: 0.5rem;
-  cursor: pointer;
-  color: #fff;
-  font-size: 1.2rem;
-  transition: all 0.3s ease;
-}
-
-.c-alpha-run-button:hover:not(.o-pelle-disabled-pointer) {
-  background: linear-gradient(135deg, #2d2d2d, #404040);
-  border-color: #666;
-}
-
-.c-alpha-run-button.o-pelle-disabled-pointer {
-  opacity: 0.5;
-  cursor: not-allowed;
-}
-
-.c-alpha-run-button__icon {
-  width: 2rem;
-  height: 2rem;
-  display: inline-block;
-  margin-left: 1rem;
-  vertical-align: middle;
-}
-
-.c-alpha-run-button__icon--running {
-  animation: pulse 2s infinite;
-}
-
-@keyframes pulse {
-  0%, 100% { opacity: 1; }
-  50% { opacity: 0.5; }
+.c-alpha-run-description {
+  width: 46rem;
 }
 </style>
 
 <template>
-  <div class="l-alpha-tab">
+  <div class="l-teresa-celestial-tab">
     <CelestialQuoteHistory celestial="alpha" />
-    <div class="l-alpha-content">
-      <button
-        class="c-alpha-run-button"
-        :class="{ 'o-pelle-disabled-pointer': isDoomed }"
+    <div class="l-alpha-run">
+      <div class="c-alpha-run-description">
+        <span :class="{ 'o-pelle-disabled': isDoomed }">
+          Enter Alpha's Reality.
+        </span>
+      </div>
+      <div
+        :class="runButtonOuterClass"
         @click="startRun"
       >
-        <b>
-          <span v-if="isRunning">You are in </span>
-          <span v-else>Start </span>
-          Alpha's Reality
-        </b>
-        <br>
+        <div
+          :class="runButtonInnerClass"
+          :button-symbol="symbol"
+        >
+          {{ symbol }}
+        </div>
+      </div>
+      <div class="c-alpha-run-description">
         {{ runDescription }}
-      </button>
+      </div>
     </div>
   </div>
 </template>
