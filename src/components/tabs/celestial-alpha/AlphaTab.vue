c3f3142e8190538e597f2b7ef31a3ea1baccfe64
Quantaum <crescentiumlmao@gmail.com>
2026-01-19 02:25:20 -0800

    Update AlphaTab.vue

--- src/components/tabs/celestial-alpha/AlphaTab.vue
+++ src/components/tabs/celestial-alpha/AlphaTab.vue
@@ -1,15 +1,19 @@
-<template>
-  <div class="l-alpha-tab">
-    <div class="l-alpha-content">
-      <h2>Alpha</h2>
-      <p>Alpha tab - Work in progress</p>
-    </div>
-  </div>
-</template>
-
 <script>
+import { DC } from "@/core/constants";
+
+import CelestialQuoteHistory from "@/components/CelestialQuoteHistory";
+import CustomizeableTooltip from "@/components/CustomizeableTooltip";
+import GlyphSetPreview from "@/components/GlyphSetPreview";
+import PrimaryButton from "@/components/PrimaryButton";
+
 export default {
   name: "AlphaTab",
+  components: {
+    PrimaryButton,
+    GlyphSetPreview,
+    CelestialQuoteHistory,
+    CustomizeableTooltip
+  },
   data() {
     return {
     };
@@ -30,3 +34,12 @@ export default {
   padding: 1rem;
 }
 </style>
+
+<template>
+  <div class="l-alpha-tab">
+    <div class="l-alpha-content">
+      <h2>Alpha</h2>
+      <p>Alpha tab - Work in progress</p>
+    </div>
+  </div>
+</template>
